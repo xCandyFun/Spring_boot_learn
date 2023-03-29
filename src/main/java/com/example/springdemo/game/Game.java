596e41a6fa8089dc4f25d0cd981d810550cba817
PC Simon <simtho0624@gmail.com>
2023-03-29 12:40:54 +0200

    Add Game class and GameRepository interface

--- src/main/java/com/example/springdemo/game/Game.java
+++ src/main/java/com/example/springdemo/game/Game.java
@@ -1,10 +1,14 @@
 package com.example.springdemo.game;
 
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 
 @Entity
